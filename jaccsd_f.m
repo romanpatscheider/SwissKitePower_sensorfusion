16b84e92bcc6286d021e3da58781712a8ea5ed0b
Roman Patscheider <romanp@student.ethz.ch>
2012-06-01 14:35:38 +0200

    ...

--- jaccsd_f.m
+++ jaccsd_f.m
@@ -1,10 +1,10 @@
-function [z,A]=jaccsd_f(fun,x,DCM_ir, DCM_br)
+function [z,A]=jaccsd_f(fun,x,DCM_ir, DCM_br,t)
 % JACCSD Jacobian through complex step differentiation
 % [z J] = jaccsd(f,s)
 % z = f(x)
 % J = f'(x)
 %
-z=fun(x,DCM_ir,DCM_br);
+z=fun(x,DCM_ir,DCM_br,t);
 n=numel(x);
 m=numel(z);
 A=zeros(m,n);
@@ -12,5 +12,5 @@ h=n*eps;
 for k=1:n
     x1=x;
     x1(k)=x1(k)+h*i;
-    A(:,k)=imag(fun(x1,DCM_ir,DCM_br))/h;
+    A(:,k)=imag(fun(x1,DCM_ir,DCM_br,t))/h;
 end
