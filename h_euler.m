ef1059fbb213c31ee2b52569420695abe9a636d8
fabian <fabianhilti@hotmail.com>
2012-06-12 17:44:31 +0200

    only euler

---
+++ h_euler.m
@@ -0,0 +1,54 @@
+function [z_est]=h_euler(x_est,x_old,t,dis,G,mag)
+
+    
+    lat = x_est(1);
+    long= x_est(2);
+    alt = x_est(3);
+    vn  = x_est(4);
+    ve  = x_est(5);
+    vd  = x_est(6);
+    phi = x_est(7);
+    thet = x_est(8);
+    pssi = x_est(9);
+    d_phi  = x_est(10);
+    d_thet  = x_est(11);
+    d_psi  = x_est(12);
+    bax = x_est(13);
+    bay = x_est(14);
+    baz = x_est(15);
+    bgx = x_est(16);
+    bgy = x_est(17);
+    bgz = x_est(18);
+    bmx = x_est(19);
+    bmy = x_est(20);
+    bmz = x_est(21);
+    vn_old=x_old(4);
+    ve_old=x_old(5);
+    vd_old=x_old(6);
+    phi_old=x_old(7);
+    thet_old=x_old(8);
+    psi_old=x_old(9);
+    d_phi_old=x_old(10);
+    d_thet_old=x_old(11);
+    d_psi_old=x_old(12);
+    
+DCM_bi=calc_DCM_br(x_est(7),x_est(8),x_est(9));
+
+w=[d_phi;0;0]+[1,0,0;0,cos(phi),sin(phi);0,-sin(phi),cos(phi)]*[0;d_thet;0] ...
+    +[1,0,0;0,cos(phi),sin(phi);0,-sin(phi),cos(phi)]...
+    *[cos(thet), 0, -sin(thet);0,1,0;sin(thet),0,cos(thet)]*[0;0;d_psi];
+w_old=[d_phi_old;0;0]+[1,0,0;0,cos(phi_old),sin(phi_old);0,-sin(phi_old),cos(phi_old)]*[0;d_thet_old;0] ...
+    +[1,0,0;0,cos(phi_old),sin(phi_old);0,-sin(phi_old),cos(phi_old)]...
+    *[cos(thet_old), 0, -sin(thet_old);0,1,0;sin(thet_old),0,cos(thet_old)]*[0;0;d_psi_old];
+vg=transp(cross(transp(w),transp(dis)));
+vg_old=transp(cross(transp(w_old),transp(dis)));
+v=[vn;ve;vd];
+v_old=[vn_old;ve_old;vd_old];
+
+h1=[lat;long;alt] + transp(DCM_bi)*dis; %pos
+h2=v + transp(DCM_bi)*vg; %vel
+h3=DCM_bi*(((v+transp(DCM_bi)*vg)-(v_old+transp(DCM_bi)*vg_old))./t-G);%acc
+h4=w;%gyr
+h5=DCM_bi*mag;%magnetometer
+
+z_est=[h1;h2;h3;h4;h5];
--- state_estimation_fmm_euler.m
+++ state_estimation_fmm_euler.m
@@ -6,20 +6,22 @@ dis=[0;0;0];%displacementvector of the box
 MAG1=0.2145;% [Gauss] magnetic field in zurich
 MAG2=0.0060;
 MAG3=0.4268;
-mag=[MAG1,MAG2,MAG3];
+   true_mag=[-0.119784172661871;-0.0330935251798561;0.341007194244604;];
+    mag_angle=-1.496;
+    mag=[cos(mag_angle),sin(mag_angle),0;-sin(mag_angle),cos(mag_angle),0;0,0,1]*true_mag;
 %Rl= geocradius(47+24/60); %from zurich
 %Rp= Rl*cos(8+32/60); %from zurich
 lat0=47+24/60;
 long0=8+32/60;
 % noise form Xsens datasheet
-NOISE_ACC_b=[0.0014;0.0014;0.0014];% [m/s^2/sqrt(Hz)]noise acc   Xsens: [0.002;0.002;0.002]
-NOISE_GYRO_b=[0.03;0.03;0.03]*2*pi/360;% [rad/s] noise gyro      Xsens: [0.05;0.05;0.05]./360.*2*pi
-NOISE_MAG_b=[0.002;0.002;0.002];%[gauss]                         Xsens: [0.5e-3;0.5e-3;0.5e-3]
 
-NOISE_GPS_POS=0.001;% Noise in position of the GPS
-NOISE_GPS_VEL=0.001;%Noise in velocity of the GPS
-noise_scale=1;
 
+NOISE_ACC_b=10*[0.14;0.14;0.14];% [m/s^2/sqrt(Hz)]noise acc   Xsens: [0.002;0.002;0.002]
+NOISE_GYRO_b=10*[0.3;0.3;0.3]*2*pi/360;% [rad/s] noise gyro      Xsens: [0.05;0.05;0.05]./360.*2*pi
+NOISE_MAG_b=1000*[0.002;0.002;0.002];%[gauss]                         Xsens: [0.5e-3;0.5e-3;0.5e-3]
+
+NOISE_GPS_POS=0.0005;% Noise in position of the GPS
+NOISE_GPS_VEL=0.005;%Noise in velocity of the GPS
 
 
 %------------------------
@@ -34,18 +36,24 @@ x = [0.451525,-0.02503,-0.7155918,...
     0,0.173924051749699,-1.73248360301804,...
     0,0,0,0,0,0,0,0,0]';
 P = zeros(size(x,1),size(x,1));
-quat = [0,0,1,0]';
 %------------------------
 % Q and R are calculated
 %------------------------
+%  q_diag=[0.001,0.001,0.001,0.01,0.01,0.01,0.0001,0.0001,0.0001,0.0001,0.0001,0.0001,0,0,0,0,0,0,0,0,0];
+%     
+%     Q=diag(q_diag);
+% 
+% 
+% r=[NOISE_GPS_POS,NOISE_GPS_POS,NOISE_GPS_POS,NOISE_GPS_VEL,NOISE_GPS_VEL,NOISE_GPS_VEL,(NOISE_ACC_b.^2)',(NOISE_GYRO_b.^2)',(NOISE_MAG_b.^2)'];
+% R=diag(r);
 
+% noise predection
+q_diag=[0.001,0.001,0.001,0.01,0.01,0.01,0.0001,0.0001,0.0001,0.0001,0.0001,0.0001,0,0,0,0,0,0,0,0,0];
+Q=diag(q_diag);
 
-
-r=[NOISE_GPS_POS,NOISE_GPS_POS,NOISE_GPS_POS,NOISE_GPS_VEL,NOISE_GPS_VEL,NOISE_GPS_VEL,(NOISE_ACC_b.^2)',(NOISE_GYRO_b.^2)',(NOISE_MAG_b.^2)'];
+% noise correction
+r=[NOISE_GPS_POS,NOISE_GPS_POS,NOISE_GPS_POS,NOISE_GPS_VEL,NOISE_GPS_VEL,NOISE_GPS_VEL,(NOISE_ACC_b)',(NOISE_GYRO_b)',(NOISE_MAG_b)'];
 R=diag(r);
-
-
-
 
 
 
@@ -63,22 +71,19 @@ while (i<size(M,2))%size(M,2)
     %------------------------
     % direct cosine matrice gets calculated
     %------------------------
-    DCM_ir=calc_DCM_ir(quat);
-    DCM_br=calc_DCM_br(x(7),x(8),x(9));
-    DCM_bi=DCM_br*DCM_ir';
+
+    DCM_bi=calc_DCM_br(x(7),x(8),x(9));
     
-    
-    %noise, dependent on DCMs
-    NOISE_ACC_i=(NOISE_ACC_b);%DCM_ir*
-    NOISE_VEL_i=[NOISE_GPS_VEL;NOISE_GPS_VEL;NOISE_GPS_VEL];
-    NOISE_POS_i=(NOISE_VEL_i.^2)*t;
-    NOISE_GYRO_i=(NOISE_GYRO_b);%DCM_ir*
-    NOISE_CARTAN_i=(NOISE_GYRO_i.^2)*t;
+%     
+%     %noise, dependent on DCMs
+%     NOISE_ACC_i=(NOISE_ACC_b);%DCM_ir*
+%     NOISE_VEL_i=[NOISE_GPS_VEL;NOISE_GPS_VEL;NOISE_GPS_VEL];
+%     NOISE_POS_i=(NOISE_VEL_i.^2)*t;
+%     NOISE_GYRO_i=(NOISE_GYRO_b);%DCM_ir*
+%     NOISE_CARTAN_i=(NOISE_GYRO_i.^2)*t;
     
     %q_diag=[NOISE_POS_i',NOISE_VEL_i',NOISE_CARTAN_i', NOISE_GYRO_i',0,0,0,0,0,0,0,0,0].*noise_scale;
-    q_diag=[0.001,0.001,0.001,0.01,0.01,0.01,0.0001,0.0001,0.0001,0.0001,0.0001,0.0001,0,0,0,0,0,0,0,0,0];
-    
-    Q=diag(q_diag);
+   
     
     
     [x_est,A]=jaccsd_free_mass_model(@free_mass_model,x,t);
@@ -99,7 +104,7 @@ while (i<size(M,2))%size(M,2)
 %     DCM_br_est=calc_DCM_br(x_est(7),x_est(8),x_est(9));
 %     DCM_bi_est=DCM_br_est*DCM_ir';
    
-    [z_est,H]=jaccsd_h(@h,x_est,x,DCM_ir,t,dis,G,mag);
+    [z_est,H]=jaccsd_h_euler(@h_euler,x_est,x,t,dis,G,mag);
     
     
     
@@ -119,9 +124,6 @@ while (i<size(M,2))%size(M,2)
     counter_old=counter(:,i_old);
     length_z=size(z_new);
     
-    DCM_br_est=calc_DCM_br(x_est(7),x_est(8),x_est(9));
-    DCM_bi=DCM_br_est*transp(DCM_ir);
-    save_mag(:,k)=DCM_bi'*z_new(13:15);
     
     meas_control= d_meas(counter_new,counter_old,length_z);
     
@@ -170,27 +172,10 @@ while (i<size(M,2))%size(M,2)
 %    save_x(:,k)=x;
     save_new(:,k)=x_new;
     save_est(:,k)=x_est;
-    save_quat(:,k)=quat;
     k=k+1;
     
-    %reset
-        
-%     DCM_br=calc_DCM_br(x_new(7),x_new(8),x_new(9));
-%     DCM_bi=DCM_br*DCM_ir';
-%     
-%     quat=DCMtoQ(DCM_bi');
-%     deuler2body=calc_deuler2body(x_new(7),x_new(8),x_new(9));
-%     
-%     
-%     x_new(7)=0;
-%     x_new(8)=0;
-%     x_new(9)=0;
-%     x_new(10:12)=DCM_br'*deuler2body*x_new(10:12);
-    
-  
     x=x_new;
-    
-    %i=i+1;
+  
     
 end
 
@@ -207,6 +192,6 @@ end
 %% pos and vel compared to ground truth
 figure(5);plot(save_time,save_x(1,:),save_time,save_new(1,:),save_time,save_est(1,:),segment1_time_ground_truth,segment1_ground_truth(1,:),save_time,save_x(4,:)/10,save_time,save_new(4,:)/10,segment1_time_ground_truth,segment1_ground_truth(4,:)/10);legend('x','new','est','ground truth','v state x','v est x','v ground truth')
 %%
-figure(7);plot(save_time,save_est(9,:),save_time,save_new(9,:),save_time,save_est(10,:),save_time,save_new(10,:));legend('acc est','acc new','gyro est','gyro new');
+figure(7);plot(save_time,save_est(9,:),save_time,save_new(9,:));legend('deuler');
 %% Quaternions
-figure(6);plot(save_time,save_quat(1,:),segment1_time_ground_truth, segment1_ground_truth(7,:));legend('quat 1','quat gt');
+figure(6);plot(save_time,save_est(9,:),save_time,save_new(9,:),segment1_time_ground_truth, -segment1_ground_truth(9,:));legend('euler x','euler est','euler gt');
