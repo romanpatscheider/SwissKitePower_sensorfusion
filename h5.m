2edf4ef221f129e22d6e798d9f5100586c2fe09b
Roman Patscheider <romanp@student.ethz.ch>
2012-06-13 11:09:46 +0200

    kei ahnig was i alles gänderet han...

--- h5.m
+++ h5.m
@@ -8,10 +8,9 @@ function [z_est]=h5(x_est,t)
     MAG2=0.0060;
     MAG3=0.4268;
     true_mag=[-0.119784172661871;-0.0330935251798561;0.341007194244604;];
-    mag_angle=-1.496;
+    mag_angle=1.496;
     mag=[cos(mag_angle),sin(mag_angle),0;-sin(mag_angle),cos(mag_angle),0;0,0,1]*true_mag;
-   
-
+    
 deuler2body=calc_deuler2body(x_est(1),x_est(2),x_est(3));
 w_body=[0,0,1;0,1,0;-1,0,0]*deuler2body*x_est(5:7);
 
@@ -48,4 +47,11 @@ h3=a_zp+a_zp_dis+a_G;
 h4=w_body;%gyr
 h5=DCM_bi*mag;%magnetometer
 
-z_est=[h1;h2;h3;h4;h5];
+DCM_b2i_n=calc_DCM_br(x_est(1),x_est(2),x_est(3));
+    DCM_br_n=[0,0,1;0,1,0;-1,0,0]*DCM_b2i_n;
+
+cardan_n=[atan2(DCM_br_n(2,3),DCM_br_n(3,3)) ...
+              asin(-DCM_br_n(1,3)) ...
+              atan2(DCM_br_n(1,2),DCM_br_n(1,1))];
+h6=cardan_n';
+z_est=[h1;h2;h3;h4;h5;h6];
