9e0cb2246460f0a711eccbfb16f55dd25cd4b5c0
Roman Patscheider <romanp@student.ethz.ch>
2012-06-28 08:34:06 +0200

    worked on report chapter 1, 3 and matlab _5 files

--- h5_x.m
+++ h5_x.m
@@ -9,7 +9,7 @@ function [z_est]=h5(x_est,t)
     R=0.8465;
     dR=0;
     G= [0;0;9.85]; % [m/s^2] gravitation in zurich
-    dis=[0;0;0];%displacementvector of the box
+    dis=[0.0;0;0];%displacementvector of the box
     MAG1=0.2145;% [Gauss] magnetic field in zurich
     MAG2=0.0060;
     MAG3=0.4268;
@@ -18,7 +18,7 @@ function [z_est]=h5(x_est,t)
 %     true_mag=[-0.119784172661871;-0.0330935251798561;0.341007194244604;];
 %     mag_angle=-1.496;
     true_mag=[-0.0375785801027502;-0.449114660422007;0.823831720011575;];
-    mag_angle=2.417-pi/2;
+    mag_angle=2.417-pi/2-0.4;
     mag=[cos(mag_angle),sin(mag_angle),0;-sin(mag_angle),cos(mag_angle),0;0,0,1]*true_mag;
 %     
 %calculate matrix to convert changes in euler angles to rotations in the
@@ -63,11 +63,14 @@ h4=w_body;%gyr
 h5=DCM_bi*mag;
 % calculate cardan angles from staes (not used for correction! -> complex
 % atan2 warning may be ignored
-DCM_b2i_n=calc_DCM_br(x_est(1),x_est(2),x_est(3));
-    DCM_br_n=[0,0,-1;0,1,0;-1,0,0]*DCM_b2i_n;
+DCM_b2i_n=calc_DCM_br(real(x_est(1)),real(x_est(2)),real(x_est(3)));
+psi=-0.2;
+thet=0.07;
+    DCM_br_n=[cos(psi),sin(psi),0;-sin(psi),cos(psi),0;0,0,1]*[cos(thet), 0, -sin(thet);0,1,0;sin(thet),0,cos(thet)]*[0,0,-1;0,1,0;1,0,0]*DCM_b2i_n;
 
-cardan_n=[atan2(DCM_br_n(2,3),DCM_br_n(3,3)) ...
-              asin(-DCM_br_n(1,3)) ...
-              atan2(DCM_br_n(1,2),DCM_br_n(1,1))];
+cardan_n=[-atan2(-DCM_br_n(3,2),-DCM_br_n(3,3))  ...
+              asin(-DCM_br_n(3,1)) ...
+              atan2(DCM_br_n(2,1),DCM_br_n(1,1))];
+
 h6=cardan_n';
 z_est=[h1;h2;h3;h4;h5;h6];
