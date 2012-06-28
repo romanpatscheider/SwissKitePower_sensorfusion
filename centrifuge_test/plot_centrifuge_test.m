d0767da6479854d127d1ded13913f091745878ad
fabian <fabianhilti@hotmail.com>
2012-06-28 10:43:39 +0200

    nothing

--- centrifuge_test/plot_centrifuge_test.m
+++ centrifuge_test/plot_centrifuge_test.m
@@ -31,3 +31,8 @@ subplot(3,1,1);plot(meas_time_P,magn_P(1,:)/1370+0.3781-0.07,PacketMagGyroAcc*t+
 subplot(3,1,2);plot(meas_time_P,magn_P(2,:)/1370+0.3781-0.07-0.13,PacketMagGyroAcc*t+60*60*13+19*60+44, -MagnetometerY0x28G0x29-0.5,meas_time_X(1,:),-(magn_X(2,:)-2^15)/15007.5);title('Magnetometer x-axis');legend('PX4','x-IMU','MTi-G');xlabel('[s]');ylabel('[gauss]');
 subplot(3,1,3);plot(meas_time_P,magn_P(3,:)/1370,PacketMagGyroAcc*t+60*60*13+19*60+44, -MagnetometerZ0x28G0x29+0.5,meas_time_X(1,:),-(magn_X(3,:)-2^15)/15007.5);title('Magnetometer x-axis');legend('PX4','x-IMU','MTi-G');xlabel('[s]','MTi-G');ylabel('[gauss]');
 
+%% sinecurve
+figure(6);
+subplot(3,1,1);plot(meas_time_P(1,:),acc_P(1,:)*9.81/2048,PacketMagGyroAcc*t+60*60*13+19*60+44, AccelerometerX0x28g0x29*9.81,meas_time_X(1,:),(acc_X(1,:)-2^15)/(100*4));title('Accelerometer x-axis');legend('PX4','x-IMU','MTi-G');xlabel('[s]');ylabel('[m/s^2]');
+subplot(3,1,2);plot(meas_time_P(1,:),gyro_P(3,:)*17.5/(1000),PacketMagGyroAcc*t+60*60*13+19*60+44, -GyroscopeZ0x28deg0x2Fs0x29,meas_time_X(1,:),-(gyro_X(3,:)-2^15)*5.7*0.889/(100*4));title('Gyroscope z-axis');legend('PX4','x-IMU','MTi-G');xlabel('[s]');ylabel('[deg/s]');
+subplot(3,1,3);plot(meas_time_P,magn_P(1,:)/1370+0.3781-0.07,PacketMagGyroAcc*t+60*60*13+19*60+44, MagnetometerX0x28G0x29-0.5,meas_time_X(1,:),(magn_X(1,:)-2^15)/15007.5);title('Magnetometer x-axis');legend('PX4','x-IMU','MTi-G');xlabel('[s]');ylabel('[gauss]');
